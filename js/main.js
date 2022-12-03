af514ff45a5368240abe0493732c66d0b0502ead
herbertribeiro19 <hebert11223@gmail.com>
2022-12-03 17:13:15 -0300

    finished

--- js/main.js
+++ js/main.js
@@ -2,4 +2,23 @@ var swiper = new Swiper(".slide-characters", {
     slidesPerView: 3.5,
     spaceBetween: 19,
     freeMode: true,
+    breakpoints: {
+        320:{
+            slidesPerView: 1.1,
+        },
+        768: {
+            slidesPerView: 2.2,
+        },
+        991: {
+            slidesPerView: 2.8,
+        },       
+        1200: {
+            slidesPerView: 3.5,
+        },
+    },
+    });
+
+AOS.init({
+    duration: 1000,
+    once: false,
 });
